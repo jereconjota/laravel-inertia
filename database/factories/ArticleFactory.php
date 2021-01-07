b9974be28bb4c795452e40306be1f04582c7161f
jereconjota <jeremiasfigueroa69@gmail.com>
2021-01-07 18:19:22 -0300

    factories bug solved

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -13,7 +13,7 @@ class ArticleFactory extends Factory
      * @var string
      */
     // protected $model = Article::class;
-    protected $model = App\Models\Article::class;
+    protected $model = Article::class;
 
     /**
      * Define the model's default state.
